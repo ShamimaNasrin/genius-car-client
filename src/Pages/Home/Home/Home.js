d146ba3eac3f690eb350d59237cc9dd9162df21c
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-03 01:13:53 +0600

    about

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import About from '../About/About';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h1>this is home</h1>
-
+            <About></About>
         </div>
     );
 };
