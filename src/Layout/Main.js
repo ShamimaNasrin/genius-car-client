c021dce447893774e228295b8d53cc44148565b5
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-03 15:19:06 +0600

    signup

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
 
 const Main = () => {
+
     return (
         <div>
             <Header></Header>
