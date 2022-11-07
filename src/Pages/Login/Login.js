f15e307ee8270add4802da0c8820a06544527f5a
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-07 21:11:35 +0600

    initial token check one

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -24,8 +24,27 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+
+                //get jwt token
+                const currentUser = {
+                    email: user.email
+                }
+                console.log(currentUser);
+
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                    })
+
                 form.reset();
-                navigate(from, {replace: true});
+                // navigate(from, {replace: true});
             })
             .catch(err => console.error(err));
 
